ddd330b8891b7b00c18b870de21430a5d6850540
Lingani Tshuma <tshumal@gmail.com>
2023-07-20 12:14:10 +0200

    Update papss-linux-installer.sh

--- linux/papss-linux-installer.sh
+++ linux/papss-linux-installer.sh
@@ -2,13 +2,13 @@ sudo groupadd -r papss
 sudo useradd -r -s /bin/false -g papss papss
 id papss
 
-sudo mv papssinbound.service /etc/systemd/system/ && sudo chmod 0644 /etc/systemd/system/papssinbound.service
-sudo mv papssoutbound.service /etc/systemd/system/ && sudo chmod 0644 /etc/systemd/system/papssoutbound.service
-sudo mv papssconfig.service /etc/systemd/system/ && sudo chmod 0644 /etc/systemd/system/papssconfig.service
+#sudo mv papssinbound.service /etc/systemd/system/ && sudo chmod 0644 /etc/systemd/system/papssinbound.service
+#sudo mv papssoutbound.service /etc/systemd/system/ && sudo chmod 0644 /etc/systemd/system/papssoutbound.service
+#sudo mv papssconfig.service /etc/systemd/system/ && sudo chmod 0644 /etc/systemd/system/papssconfig.service
 
-sudo chown -R root:root /etc/systemd/system/papssinbound.service
-sudo chown -R root:root /etc/systemd/system/papssoutbound.service
-sudo chown -R root:root /etc/systemd/system/papssconfig.service
+#sudo chown -R root:root /etc/systemd/system/papssinbound.service
+#sudo chown -R root:root /etc/systemd/system/papssoutbound.service
+#sudo chown -R root:root /etc/systemd/system/papssconfig.service
 
 sudo chown -R papss:papss /app/papss
 sudo systemctl daemon-reload
